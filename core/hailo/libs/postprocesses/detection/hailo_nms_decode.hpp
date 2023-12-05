cc19aa2e29d67f682defd6bd8937a4d96cc0241a
oepl <oepl.cm@optimizedelectrotech.com>
2023-12-05 15:58:13 +0530

    detection multi threshold post process

--- core/hailo/libs/postprocesses/detection/hailo_nms_decode.hpp
+++ core/hailo/libs/postprocesses/detection/hailo_nms_decode.hpp
@@ -16,6 +16,17 @@
 static const int DEFAULT_MAX_BOXES = 100;
 static const float DEFAULT_THRESHOLD = 0.4;
 
+#define YOLO_SHM_KEY 0x1222
+struct yolo_shmseg {
+    float detectThresh1=0.5;
+    float detectThresh2=0.25;
+    float detectThresh3=0.1;
+    unsigned int rectAreaThresh1=1024;
+    unsigned int rectAreaThresh2=400;
+    unsigned int model_input_size_x=1280;
+    unsigned int model_input_size_y=768;
+};
+
 class HailoNMSDecode
 {
 private:
@@ -25,6 +36,9 @@ private:
     uint _max_boxes;
     bool _filter_by_score;
     const hailo_vstream_info_t _vstream_info;
+
+    int yolo_shmid;
+    struct yolo_shmseg *yolo_shmp;
 
     common::hailo_bbox_float32_t dequantize_hailo_bbox(const auto *bbox_struct)
     {
@@ -42,15 +56,46 @@ private:
     void parse_bbox_to_detection_object(auto dequant_bbox, uint32_t class_index, std::vector<HailoDetection> &_objects)
     {
         float confidence = CLAMP(dequant_bbox.score, 0.0f, 1.0f);
-        // filter score by detection threshold if needed.
-        if (!_filter_by_score || dequant_bbox.score > _detection_thr)
-        {
-            float32_t w, h = 0.0f;
-            // parse width and height of the box
-            std::tie(w, h) = get_shape(&dequant_bbox);
-            // create new detection object and add it to the vector of detections
-            _objects.push_back(HailoDetection(HailoBBox(dequant_bbox.x_min, dequant_bbox.y_min, w, h), class_index, labels_dict[class_index], confidence));
-        }
+
+        //// filter score by detection threshold if needed.
+        //if (!_filter_by_score || dequant_bbox.score > _detection_thr)
+        //{
+        //    float32_t w, h = 0.0f;
+        //    // parse width and height of the box
+        //    std::tie(w, h) = get_shape(&dequant_bbox);
+        //    // create new detection object and add it to the vector of detections
+        //    _objects.push_back(HailoDetection(HailoBBox(dequant_bbox.x_min, dequant_bbox.y_min, w, h), class_index, labels_dict[class_index], confidence));
+        //}
+
+
+	float32_t w, h = 0.0f;        
+	std::tie(w, h) = get_shape(&dequant_bbox); // parse width and height of the box   
+	unsigned int area=(unsigned int)(yolo_shmp->model_input_size_x*yolo_shmp->model_input_size_y*w*h);
+
+	if(area <= yolo_shmp->rectAreaThresh2)	// Smallest detection
+	{
+		if(confidence  >= yolo_shmp->detectThresh3)
+		{
+                	_objects.push_back(HailoDetection(HailoBBox(dequant_bbox.x_min, dequant_bbox.y_min, w, h), class_index, labels_dict[class_index], confidence));
+
+		}
+	}
+	else if( area <= yolo_shmp->rectAreaThresh1 )	// Medium size detection
+	{
+		if(confidence  >= yolo_shmp->detectThresh2)
+ 		{
+               		_objects.push_back(HailoDetection(HailoBBox(dequant_bbox.x_min, dequant_bbox.y_min, w, h), class_index, labels_dict[class_index], confidence));
+
+		}
+	}
+	else
+	{
+		if(confidence  >= yolo_shmp->detectThresh1)
+		{
+	      		 _objects.push_back(HailoDetection(HailoBBox(dequant_bbox.x_min, dequant_bbox.y_min, w, h), class_index, labels_dict[class_index], confidence));
+		}
+	}
+        
     }
 
     std::pair<float, float> get_shape(auto *bbox_struct)
@@ -67,7 +112,27 @@ public:
         // making sure that the network's output is indeed an NMS type, by checking the order type value included in the metadata
         if (HAILO_FORMAT_ORDER_HAILO_NMS != _vstream_info.format.order)
             throw std::invalid_argument("Output tensor " + _nms_output_tensor->name() + " is not an NMS type");
+		
+	//Shared memory yolo postprocess
+        yolo_shmid = shmget(YOLO_SHM_KEY, sizeof(struct yolo_shmseg), 0644|IPC_CREAT); //create shared memory
+       	if (yolo_shmid == -1) 
+        {
+       	     perror("yolo post process:nms | Shared memory create error\n");
+        } 	   
+        yolo_shmp = (yolo_shmseg*)shmat(yolo_shmid, NULL, 0);//Attach to the segment to get a pointer to it.
+       	if (yolo_shmp == (void *) -1) 
+        {
+            perror("yolo post process:nms | Shared memory attach error\n");
+        }
     };
+    virtual ~HailoNMSDecode()
+    {
+	//detach shared memory
+        if (shmdt(yolo_shmp) == -1) 
+        {
+            perror("yolo post process:nms | Shared memory detach error\n");
+        }
+    }
 
     template <typename T, typename BBoxType>
     std::vector<HailoDetection> decode()
